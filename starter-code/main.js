2dd444aed7df29a7ea59279836837c5de2b97496
myztajay <myztajay@hotmail.com>
2016-11-11 10:47:30 -0500

    Added boolean if else statment to check if cards have similar values

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,10 @@ cardOne = "queen"
 cardTwo = "queen"
 cardThree = "king"
 cardFour = "king"
+
+if (cardOne === cardTwo || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree || cardTwo === cardFour || cardThree === cardFour){
+  alert("You found a match!")
+}
+else {
+  alert("Sorry, Try again?")
+}

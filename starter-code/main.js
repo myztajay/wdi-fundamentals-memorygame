6dcc0b5f7e7bf8881958d8b4ba54879684011f6d
myztajay <myztajay@hotmail.com>
2016-11-12 17:51:30 -0500

    created card object and an iterator function  to make 4 cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-cardOne = "queen"
-cardTwo = "queen"
-cardThree = "king"
-cardFour = "king"
+cardOne = "queen";
+cardTwo = "queen";
+cardThree = "king";
+cardFour = "king";
 
-if (cardOne === cardTwo || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree || cardTwo === cardFour || cardThree === cardFour){
-  alert("You found a match!")
+// if (cardOne === cardTwo || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree || cardTwo === cardFour || cardThree === cardFour){
+//   alert("You found a match!")
+// }
+// else {
+//   alert("Sorry, Try again?")
+// }
+
+var gameBoard = document.getElementById("game-board");
+
+for (var i=0; i<4; i++){
+  createCards()
 }
-else {
-  alert("Sorry, Try again?")
+
+function createCards(){
+  var card = document.createElement('div');
+  gameBoard.appendChild(card)
+  card.className = "card";
 }
